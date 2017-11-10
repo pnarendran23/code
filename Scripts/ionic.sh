1d5993c940e18a922d5a286c881c934c5d791178
Pradheep Narendran <pnarendran@usgbc.org>
2017-11-10 12:56:20 +0530

    Update ionic.sh

--- Scripts/ionic.sh
+++ Scripts/ionic.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 npm install -g ionic 2>&1
+mkdir -p /home/ubuntu/charts/
 cd /home/ubuntu/charts/ 2>&1
 rm -r node_modules 2>&1
 npm install 2>&1
 sudo npm rebuild node-sass 2>&1
 cd /home/ubuntu/charts/ 2>&1
-npm i -g cordova
+npm i -g cordova 2>&1
 ionic build 2>&1
