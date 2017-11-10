94728edf02eb0ca7d4314f782fb662d52318eb38
Pradheep Narendran <pnarendran@usgbc.org>
2017-11-10 09:10:06 +0530

    Update ionic.sh

--- Scripts/ionic.sh
+++ Scripts/ionic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-npm install -g ionic
-cd /home/ubuntu/charts/
-rm -r node_modules
-npm install
-ionic build
+npm install -g ionic 2>&1
+cd /home/ubuntu/charts/ 2>&1
+rm -r node_modules 2>&1
+npm install 2>&1
+ionic build 2>&1
