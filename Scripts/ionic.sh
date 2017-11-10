d9f1294b251c5f182c8e52d6c48011216b816d53
Pradheep Narendran <pnarendran@usgbc.org>
2017-11-10 15:30:54 +0530

    Update ionic.sh

--- Scripts/ionic.sh
+++ Scripts/ionic.sh
@@ -4,4 +4,7 @@ cd /home/ubuntu/charts/
 rm -r node_modules 2>&1
 npm install 2>&1
 sudo npm rebuild node-sass 2>&1
+apt install awscli 2>&1
+pip install -U awscli 2>&1
+aws s3 cp /home/ubuntu/charts/www/ s3://charts-ionic-dev-us-east-1/files_from_ec2 --recursive 
 ionic build 2>&1
